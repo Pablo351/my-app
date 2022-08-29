cded47d1d46d343c29acb7cc2a2745c2689d2504
Pablo351 <paex94@gmail.com>
2022-08-28 23:01:35 -0300

    Actualizacion 08.2022

--- src/Componentes/Home/Home.js
+++ src/Componentes/Home/Home.js
@@ -3,6 +3,8 @@ import Carousel from 'react-bootstrap/Carousel';
 import Image1 from '../../assets/images/FullSlide.jpg';
 import Image2 from '../../assets/images/AnalistaSlide.jpg';
 import Image3 from '../../assets/images/GenexusSlide.jpg';
+import NodeJSCert from '../../assets/images/slider002.jpg';
+
 
 
 const Home = () =>{
@@ -18,15 +20,24 @@ const Home = () =>{
                 </Carousel.Item>
 
                 <Carousel.Item className='item'>
-                    <img className="imagenSlider" src={Image2} alt="Second slide" />
+                    <img className="imagenSlider" src={NodeJSCert} alt="Second slide" />
                     <Carousel.Caption className='Contenido'>
-                        <h2>Analista en <br></br>  Informatica</h2>
-                        <p>Titulo intermedio obtenido en UES 21. <br></br> Carrera de grado en curso: "Licenciatura en Informatica"</p>
+                        <h2>NodeJs <br></br>BackEnd Developer</h2> 
+                        <p>Certificacion brindada por la Empresa Incluit</p>
+                        <a href="https://incluit.com/" rel="noreferrer" target="_blank">IncluIT</a>   
                     </Carousel.Caption>
                 </Carousel.Item>
 
                 <Carousel.Item className='item'>
-                    <img className="imagenSlider" src={Image3} alt="Third slide" />
+                    <img className="imagenSlider" src={Image2} alt="Third slide" />
+                    <Carousel.Caption className='Contenido'>
+                        <h2>Licenciatura en <br></br>  Informática</h2>
+                        <p>Estudiante universitario en UES 21. <br></br> En Curso. Tercer Año completo </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item className='item'>
+                    <img className="imagenSlider" src={Image3} alt="Fourth slide" />
                     <Carousel.Caption className='Contenido'>
                         <h2>Genexus Developer</h2>
                         <p>Actualmente desarrolllando en Tekhne. Desarrollo del software para APROSS</p>
--- src/Componentes/Skills/Skills.js
+++ src/Componentes/Skills/Skills.js
@@ -9,23 +9,25 @@ import SASS from '../../assets/images/SASS.png';
 import REACT from '../../assets/images/REACT.png';
 import SQL from '../../assets/images/SQL.png';
 import GENEXUS from '../../assets/images/GENEXUS.jpg';
+import NODEJS from '../../assets/images/9035101_logo_nodejs_icon.png'
 
 const Skills = () =>{
     return(
         <div className='skills' id='skills'>
             <h5>Poseo conocimientos en:</h5>
             <div className='imagenesSkills'>
-                <img src={HTML} alt= 'HTML' title='HTML' />
-                <img src={JAVA}alt= 'JAVA' title='JAVA'/>
-                <img src={PHP}alt= 'PHP' title='PHP'/>
-                <img src={JS}alt= 'JS' title='Java Script'/>
-                <img src={Bootstrap}alt= 'Bootstrap' title='Bootstrap'/>
-                <img src={Laravel}alt= 'Laravel' title='Laravel'/>
-                <img src={CSS}alt= 'CSS' title='CSS'/>
-                <img src={SASS}alt= 'SASS' title='SASS'/>
-                <img src={REACT}alt= 'REACT' title='REACT'/>
-                <img src={SQL}alt= 'SQL' title='SQL PL/SQL'/>
-                <img src={GENEXUS}alt= 'GENEXUS' title='GENEXUS'/>
+                <img src={HTML} alt= 'HTML' title='HTML' className='img grow'/>
+                <img src={CSS}alt= 'CSS' title='CSS' className='img grow'/>
+                <img src={NODEJS} alt= 'NODEJS' title='NODEJS' className='img grow'/>
+                <img src={JS}alt= 'JS' title='Java Script' className='img grow'/>
+                <img src={JAVA}alt= 'JAVA' title='JAVA' className='img grow'/>
+                <img src={PHP}alt= 'PHP' title='PHP' className='img grow'/>
+                <img src={Bootstrap}alt= 'Bootstrap' title='Bootstrap' className='img grow'/>
+                <img src={Laravel}alt= 'Laravel' title='Laravel' className='img grow'/>           
+                <img src={SASS}alt= 'SASS' title='SASS' className='img grow'/>
+                <img src={REACT}alt= 'REACT' title='REACT' className='img grow'/>
+                <img src={SQL}alt= 'SQL' title='SQL PL/SQL' className='img grow'/>
+                <img src={GENEXUS}alt= 'GENEXUS' title='GENEXUS' className='img grow'/>
 
             </div>
 
